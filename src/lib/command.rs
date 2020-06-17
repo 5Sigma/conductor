f5897eceb35c81629ed913906af9e737e59b11f2
Joe Bellus <joe@5sigma.io>
2020-06-17 00:29:51 -0400

    indent changes

--- src/lib/command.rs
+++ src/lib/command.rs
@@ -4,28 +4,28 @@ use std::fmt;
 
 #[derive(Clone, Deserialize, PartialEq)]
 pub struct Command {
-    pub command: String,
-    #[serde(default)]
-    pub args: Vec<String>,
-    #[serde(default)]
-    pub dir: Option<String>,
-    #[serde(default)]
-    pub env: HashMap<String, String>,
+  pub command: String,
+  #[serde(default)]
+  pub args: Vec<String>,
+  #[serde(default)]
+  pub dir: Option<String>,
+  #[serde(default)]
+  pub env: HashMap<String, String>,
 }
 
 impl fmt::Display for Command {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {}", self.command, self.args.join(" "))
-    }
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{} {}", self.command, self.args.join(" "))
+  }
 }
 
 impl Default for Command {
-    fn default() -> Self {
-        Command {
-            command: "".into(),
-            args: vec![],
-            dir: None,
-            env: HashMap::new(),
-        }
+  fn default() -> Self {
+    Command {
+      command: "".into(),
+      args: vec![],
+      dir: None,
+      env: HashMap::new(),
     }
+  }
 }
--- src/lib/project.rs
+++ src/lib/project.rs
@@ -6,27 +6,27 @@ use std::path::PathBuf;
 
 #[derive(Deserialize, PartialEq)]
 pub struct Project {
-    #[serde(default)]
-    pub name: String,
-    #[serde(default)]
-    pub components: Vec<Component>,
+  #[serde(default)]
+  pub name: String,
+  #[serde(default)]
+  pub components: Vec<Component>,
 }
 
 impl Project {
-    pub fn load(path: &PathBuf) -> Result<Self, std::io::Error> {
-        let config = fs::read_to_string(path)?;
-        let p = serde_yaml::from_str::<Project>(&config)
-            .map_err(|e| Error::new(ErrorKind::Other, e))?;
+  pub fn load(path: &PathBuf) -> Result<Self, std::io::Error> {
+    let config = fs::read_to_string(path)?;
+    let p =
+      serde_yaml::from_str::<Project>(&config).map_err(|e| Error::new(ErrorKind::Other, e))?;
 
-        Ok(p)
-    }
+    Ok(p)
+  }
 }
 
 impl Default for Project {
-    fn default() -> Self {
-        Project {
-            name: "Unnamed Project".into(),
-            components: vec![],
-        }
+  fn default() -> Self {
+    Project {
+      name: "Unnamed Project".into(),
+      components: vec![],
     }
+  }
 }
